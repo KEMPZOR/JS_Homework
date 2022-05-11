08e7a9860ce912cf0d80a3b2f4d802cae917b60a
Maxim Plotnikau <zor97club@gmail.com>
2022-05-11 09:28:55 +0300

    Homework 6

---
+++ Lesson-8/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	function toggleMenu() {
+		$('.menu-toggle').toggleClass('menu-toggle_active');
+		$('.header-top__mobile,.header-top__mobile-shadow').toggleClass('active');
+	}
+
+	$('.menu-toggle,.header-top__mobile-shadow').click(function() { toggleMenu() })
+
+	function loadUser() {
+        var nextContent = $('.main-section__content');
+
+		$.ajax({
+			url: 'https://randomuser.me/api/',
+			dataType: 'json',
+			success: function (data) {
+				nextContent
+					.append('<div class=\'main-section__content-item\'>' +
+						'<div class=\'main-section__content-left\'>' +
+						'<img src=' + 
+						data.results[0].picture.large + 
+						'></div>' +
+						'<div class=\'main-section__content-right\'>' +
+						'<div class=\'main-section__content-info\'>' +
+						'<div class=\'main-section__content-author\'>' + 
+						data.results[0].name.first + 
+						'</div>' +
+						'<div class=\'main-section__content-status\'>Member</div>' +
+						'<div class=\'main-section__content-infoblock\'><div class=\'main-section__content-comment\'>' + 
+						parseInt(Math.random() * (100 - 1) + 1) +  
+						'</div> ' +
+						'<div class=\'main-section__content-time\'>20m</div></div></div>' +
+						'<div class=\'main-section__content-heading\'><a href=\'#\'>' + 
+						data.results[0].location.city + 
+						'</a></div>' +
+						'<div class=\'main-section__content-text\'>Suspendisse potenti. Duis imperdiet ex non leo condimentum efficitur. Sed rhoncus eleifend lacus sit amet faucibus. Nunc semper nisl a dolor pretium dapibus quis quis lacus. Nulla faucibus viverra laoreet. Fusce in scelerisque magna. Duis porta erat vitae lectus lacinia tempus.</div>' +
+						'<div class="main-section__content-tag"><a href="#">tag1</a>,<a href="#">tag2</a>,<a href="#">tag3</a></div></div>');
+			}
+		});
+	}
+
+	$('.main-section__pagination-button.next').click(function(e) { e.preventDefault(); loadUser() })
+
+})
