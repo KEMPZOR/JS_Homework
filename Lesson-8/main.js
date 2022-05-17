27048b7ee3f8537fb318909d19ffc3c83d7c2b8a
Maxim Plotnikau <zor97club@gmail.com>
2022-05-17 23:37:48 +0300

    Homework 6

--- Lesson-8/main.js
+++ Lesson-8/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		$('.header-top__mobile,.header-top__mobile-shadow').toggleClass('active');
 	}
 
-	$('.menu-toggle,.header-top__mobile-shadow').click(function() { toggleMenu() })
+	$('.menu-toggle,.header-top__mobile-shadow').click(function() { toggleMenu() });
 
 	function loadUser() {
         var nextContent = $('.main-section__content');
@@ -18,8 +18,8 @@ $(document).ready(function() {
 						nextContent
 						.append('<div class=\'main-section__content-item\'>' +
 							'<div class=\'main-section__content-left\'>' +
-							'<img src=' + 
-							data.results[i].picture.large + 
+							'<img alt=' + data.results[i].login.username + ' src=' +
+							data.results[i].picture.large +
 							'></div>' +
 							'<div class=\'main-section__content-right\'>' +
 							'<div class=\'main-section__content-info\'>' +
@@ -27,21 +27,23 @@ $(document).ready(function() {
 							data.results[i].name.first + 
 							'</div>' +
 							'<div class=\'main-section__content-status\'>Member</div>' +
-							'<div class=\'main-section__content-infoblock\'><div class=\'main-section__content-comment\'>' + 
-							parseInt(Math.random() * (100 - 1) + 1) +  
+							'<div class=\'main-section__content-infoblock\'>' +
+							'<div class=\'main-section__content-comment\'>' +
+							parseInt(Math.random() * (100 - 1) + 1) +
 							'</div> ' +
 							'<div class=\'main-section__content-time\'>20m</div></div></div>' +
 							'<div class=\'main-section__content-heading\'><a href=\'#\'>' + 
 							data.results[i].location.city + 
 							'</a></div>' +
 							'<div class=\'main-section__content-text\'>Suspendisse potenti. Duis imperdiet ex non leo condimentum efficitur. Sed rhoncus eleifend lacus sit amet faucibus. Nunc semper nisl a dolor pretium dapibus quis quis lacus. Nulla faucibus viverra laoreet. Fusce in scelerisque magna. Duis porta erat vitae lectus lacinia tempus.</div>' +
-							'<div class="main-section__content-tag"> <a href="#">tag1</a>,<a href="#">tag2</a>,<a href="#">tag3</a></div></div>');
+							'<div class=\'main-section__content-tag\'>' +
+							'<a href=\'#\'>tag1</a>,<a href=\'#\'>tag2</a>,<a href=\'#\'>tag3</a></div></div>');
 					}
 				}
 			}
 		});
 	}
 
-	$('.main-section__pagination-button.next').click(function(e) { e.preventDefault(); loadUser() })
+	$('.main-section__pagination-button.next').click(function(e) { e.preventDefault(); loadUser() });
 
 })
