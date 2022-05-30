c87030811700eea08d24c92b26465ccf1fe6bede
Maxim Plotnikau <zor97club@gmail.com>
2022-05-30 10:27:43 +0300

    Homework 11

--- Lesson-12/JS HTML/main.js
+++ Lesson-12/JS HTML/main.js
@@ -4,8 +4,8 @@ var container = document.getElementById('container'),
     parTwo = document.createElement('p');
 
 
-parOne.innerHTML = 'Первый текст и <a href="https://craftmebel.by/">ссылка</a> внутри <a href="https://www.onliner.by/">ссылка 2</a>';
-parTwo.innerHTML = 'Второй текст и <a href="https://www.google.com/">ссылка</a> внутри <a href="https://mebel-market.by/">ссылка 2</a>';
+parOne.innerHTML = 'Hello, here are <a href="https://www.google.by/">Link 1</a> and <a href="https://www.google.com/">Link 1</a>';
+parTwo.innerHTML = 'Hello, here are <a href="https://www.google.by/">Link 3</a> and <a href="https://www.google.com/">Link 4</a> and <a href="https://www.google.ua/">Link 5</a>';
 
 container.appendChild(parOne);
 container.appendChild(parTwo);
