e6a8a126fb8150539c1acc2b9f3684a74f5d4531
Maxim Plotnikau <zor97club@gmail.com>
2022-06-06 02:07:57 +0300

    Homework 14

--- Lesson-14/homework-14.js
+++ Lesson-14/homework-14.js
@@ -57,3 +57,35 @@ clonedObj.array.push(2);
 
 console.log(initialObj);
 console.log(clonedObj);
+
+
+/*
+Задание 2:
+    Написать функцию глубокого сравнения объектов, возвращающую boolean. Сравниваться должны значения всех типов данных
+    (+ массивы и функции; NaN не учитывать), а также любого уровня вложенности. Для определения длины объектов
+    разрешается использовать метод Object.keys(). Хорошо протестировать работу функции (можно на объекте из пред. задания).
+*/
+
+function deepCompare(obj1, obj2) {
+    if (obj1 === obj2) {
+      return true;
+    }
+
+    if ((typeof obj1 === 'object' && obj1 !== null) && (typeof obj2 === 'object' && obj2 !== null)) {
+        if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+            return false;
+        }
+        
+        for (var key in obj1) {
+            if (!deepCompare(obj1[key], obj2[key])) {
+                return false;
+            }
+        }
+      
+        return true;
+    }
+
+    return false;
+}
+  
+deepCompare(initialObj, clonedObj);
